bf35298406f5625d82571f711dc8f3c7f7e79190
youness5422 <128231906+youness5422@users.noreply.github.com>
2025-06-03 18:35:10 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: '#fff8f1',
+          100: '#feecdc',
+          200: '#fcd9bd',
+          300: '#fdba74',
+          400: '#fb923c',
+          500: '#f97316',
+          600: '#ea580c',
+          700: '#c2410c',
+          800: '#9a3412',
+          900: '#7c2d12',
+        },
+        secondary: {
+          50: '#f0fdfa',
+          100: '#ccfbf1',
+          200: '#99f6e4',
+          300: '#5eead4',
+          400: '#2dd4bf',
+          500: '#14b8a6',
+          600: '#0d9488',
+          700: '#0f766e',
+          800: '#115e59',
+          900: '#134e4a',
+        },
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+        arabic: ['Noto Sans Arabic', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
